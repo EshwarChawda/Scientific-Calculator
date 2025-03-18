7aa794872b7c465181afad98c5d4a67e09ced48c
eshwar <eshwarchawda08@gmail.com>
2025-03-18 22:06:25 +0530

    test

--- src/main/java/com/calculator/CalculatorApp.java
+++ src/main/java/com/calculator/CalculatorApp.java
@@ -1,7 +1,6 @@
 package com.calculator;
 
 import java.util.Scanner;
-//test
 public class CalculatorApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
